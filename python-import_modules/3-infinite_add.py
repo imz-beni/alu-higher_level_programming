9bf0b1d1fa173e7d958058a8962b21b9406c5abe
imz-beni <b.imanzi@alustudent.com>
2025-10-17 07:24:48 -0700

    task completion

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == "__main__":
+def infinite_add():
     total = 0
     for arg in sys.argv[1:]:
         total += int(arg)
     print(total)
+
+if __name__ == "__main__":
+    infinite_add()
